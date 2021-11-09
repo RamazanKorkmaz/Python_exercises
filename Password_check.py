fe4b8ead581c569c3b5ac827e46967ee65b23477
Ramazan Korkmaz <40044542+RamazanKorkmaz@users.noreply.github.com>
2021-11-09 11:03:39 +0100

    Add files via upload

---
+++ Password_check.py
@@ -0,0 +1,6 @@
+first_name = input("Enter first name: ")
+name = "Ramazan"
+password = "WQ12"
+if first_name.title() == name:
+    print(f"Hello, {first_name.title()}! The pasword is : {password}")
+else: print(f"Hello, {first_name}! See you later")
