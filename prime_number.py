14789c9cfb96d568cacd8fd9a91c45400d3760d8
Ramazan Korkmaz <40044542+RamazanKorkmaz@users.noreply.github.com>
2021-11-23 16:26:28 +0100

    Update prime_number.py
    if statement updated

--- prime_number.py
+++ prime_number.py
@@ -6,7 +6,7 @@ while True:
         for i in range(1, int(number) + 1):
             if int(number) % i == 0:
                 divisors_list.append(i)
-        if 1 and int(number) in divisors_list and len(divisors_list) < 3:
+        if int(number) not in (0,1) and len(divisors_list) < 3:
             prime = True   # there are max. 2 divisors for a prime number. one of them is 1 the other one is the number itself
         break
     else:
